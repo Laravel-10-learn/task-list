525dde7260239d4dc2da531f882ae919d32c6e55
Reverie <XReverieX@thenewslens.com>
2024-01-23 19:01:54 +0800

    index and show.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,39 +3,14 @@
 </h1>
 
 <div>
-    @if(count($tasks))
-        <table>
-            <thead>
-            <tr>
-                <th>id</th>
-                <th>title</th>
-                <th>description</th>
-                <th>long description</th>
-
-                <th>completed</th>
-                <th>created at</th>
-                <th>updated at</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($tasks as $task)
-                <tr>
-                    <th>{{$task->id}}</th>
-                    <th>{{$task->title}}</th>
-                    <th>{{$task->description}}</th>
-                    <th>{{$task->long_description}}</th>
-                    <th>{{$task->completed}}</th>
-                    <th>{{$task->created_at}}</th>
-                    <th>{{$task->updated_at}}</th>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
-
-    @else
+    @forelse($tasks as $task)
         <div>
-            There are no tasks!
+            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{$task->title}}</a>
         </div>
-    @endif
+
+    @empty
+        <div>There are no tasks!</div>
+    @endforelse
 </div>
+
 
