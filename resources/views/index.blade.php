2a4afabb7d2ce8ce6680bdcd0109a0fdae9d1dd8
Reverie <XReverieX@thenewslens.com>
2024-02-17 12:26:49 +0800

    24. Layouts Using Template Inheritance

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,14 @@
-<h1>
-    The task lists.
-</h1>
+@extends('layouts.app')
+@section('title', 'The task lists.')
+@section('content')
+    <div>
+        @forelse($tasks as $task)
+            <div>
+                <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+            </div>
 
-<div>
-    @forelse($tasks as $task)
-        <div>
-            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{$task->title}}</a>
-        </div>
-
-    @empty
-        <div>There are no tasks!</div>
-    @endforelse
-</div>
-
-
+        @empty
+            <div>There are no tasks!</div>
+        @endforelse
+    </div>
+@endsection
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,4 +1,14 @@
-<h1>{{$task->title}}</h1>
-<div>
-    {{ $task->title }}
-</div>
+@extends('layouts.app')
+
+@section('title', $task->title)
+
+@section('content')
+    <p>{{ $task->description }}</p>
+
+    @if($task->long_description)
+        <p>{{ $task->long_description }}</p>
+    @endif
+
+    <p>{{ $task->created_at }}</p>
+    <p>{{ $task->updated_at }}</p>
+@endsection
