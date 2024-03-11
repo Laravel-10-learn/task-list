ba4bf885414b1b0ddaf40d23566829b06acdf55e
Reverie <XReverieX@thenewslens.com>
2024-03-11 16:57:20 +0800

    36. Deleting Data

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,12 @@ Route::put('/tasks/{task}', function (Task $task, TaskRequest $request){
         ->with('success', 'Task updated successfully!');
 })->name('tasks.update');
 
+Route::delete('/task/{task}', function (Task $task) {
+    $task->delete();
+
+    return redirect()->route('tasks.index')->with('success', 'Task deleted successfully!');
+})->name('tasks.destroy');
+
 Route::get('/tasks/{task}/edit', function (Task $task) {
     return view('edit', [
         'task' => $task
